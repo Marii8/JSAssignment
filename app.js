522ebb7643f194bc9afe2de5fea28bb3a61a3b0b
Mariko Aburai <aburmari@gmail.com>
2018-11-06 21:31:30 +0900

    htmlのベース完成、header要素、swiperの枠追加

---
+++ app.js
@@ -0,0 +1,25 @@
+let mySwiper = new Swiper('.swiper-container', {
+        // Optional parameters 他にもパラメタをつけたいときは、APIのeffectで確認できる。
+        direction: 'vertical',
+        loop: true,
+        effect: 'coverflow',
+        autoHeight: true,
+        stopOnLastSlide: true,
+        direction: 'horizontal',
+
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      })
