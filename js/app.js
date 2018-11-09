ae8b7a78ae6fb555729703e8c444b13b64a12e11
Mariko Aburai <aburmari@gmail.com>
2018-11-09 16:29:56 +0900

    Swiperの横幅とSlideのpadding指定+box-sizingで調整

---
+++ js/app.js
@@ -0,0 +1,44 @@
+new Swiper('.swiper-container', {
+        // Optional parameters 他にもパラメタをつけたいときは、APIのeffectで確認できる。
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 3,
+
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+        },
+
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+
+        // And if we need scrollbar
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      });
+
+
+
+$(function(){
+
+  let contact = $('#contact');
+  let modalBg = $('#modal-bg');
+  let modal = $('.modal-content');
+  let x = $('.js-modal-close');
+
+  contact.on('click', function() {
+    console.log(1);
+    modalBg.fadeIn(1500);
+    modal.fadeIn(1500);
+  });
+
+  x.on('click', function() {
+    modalBg.fadeOut(500);
+    modal.fadeOut(500);
+  });
+
+}); //jQueryのおまじない閉じタグ
